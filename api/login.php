ade0e347d92503e76e4bef715a2c5affcb01df6f
Jean-Marie04355 <oktcjean@gmail.com>
2025-07-18 13:44:07 +0100

    modification

--- api/login.php
+++ api/login.php
@@ -1,3 +1,4 @@
+
 <?php
 header('Content-Type: application/json');
 require_once 'config.php';
@@ -11,7 +12,7 @@ if (empty($email) || empty($mot_de_passe)) {
 }
 
 try {
-    $stmt = $pdo->prepare("SELECT id, nom, prenom, email, mot_de_passe, photo_profil FROM users WHERE email = ?");
+    $stmt = $pdo->prepare("SELECT id, nom, prenom, email, mot_de_passe, photo_profil, user_role FROM users WHERE email = ?");
     $stmt->execute([$email]);
     $user = $stmt->fetch();
 
@@ -24,13 +25,13 @@ try {
                 'nom' => $user['nom'],
                 'prenom' => $user['prenom'],
                 'email' => $user['email'],
-                'photo_profil' => $user['photo_profil']
+                'photo_profil' => $user['photo_profil'],
+                'role' => $user['user_role']
             ]
         ]);
     } else {
         echo json_encode(['success' => false, 'message' => 'Email ou mot de passe incorrect.']);
     }
-
 } catch (Exception $e) {
     echo json_encode(['success' => false, 'message' => 'Erreur serveur.']);
 }
